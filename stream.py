1e14fd4ab5ba3196e0bf0ca78852d4748935fa12
Bora Kaya <kayabo19@itu.edu.tr>
2024-11-17 02:06:49 +0300

    .

--- stream.py
+++ stream.py
@@ -716,7 +716,7 @@ if page=="Harcama Grupları":
         y=grouped.index[i], 
         text=f"{value:.2f}%", 
         showarrow=False, 
-        font=dict(size=14, family="Arial Black", weight="bold"),  # Etiketler için yazı tipi
+        font=dict(size=14, family="Arial Black"),  # Etiketler için yazı tipi
         align='left', 
         xanchor='left', 
         yanchor='middle'
