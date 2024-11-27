6584e4f392e71ff4d51f9a215ccc85f59d288965
Bora Kaya <kayabo19@itu.edu.tr>
2024-11-27 19:05:56 +0300

    .

--- stream.py
+++ stream.py
@@ -411,8 +411,8 @@ if page=="Gıda Fiyat Endeksi":
         # X ekseninde özelleştirilmiş tarih etiketlerini ayarlıyoruz
     figgalt.update_layout(
             xaxis=dict(
-                tickvals=selected_group_data.index[::3],  # Original datetime index
-                ticktext=selected_group_data.index.strftime("%d.%m.%Y"),  # Custom formatted labels
+                tickvals=selected_group_data.index,  # Original datetime index
+                ticktext=selected_group_data.index[::3].strftime("%d.%m.%Y"),  # Custom formatted labels
                 tickfont=dict(size=14, family="Arial Black", color="black")
             ),
             yaxis=dict(
