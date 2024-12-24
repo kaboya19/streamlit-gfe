48f669b7c692b823da901934e052b6b962279b88
Bora Kaya <kayabo19@itu.edu.tr>
2024-12-24 11:22:07 +0300

    .

--- stream.py
+++ stream.py
@@ -192,7 +192,7 @@ if page=="Bültenler":
         # Küçük boyutta uyarı metni
         st.markdown("""
         <small>*Bu bültenin bir sonraki yayınlanma tarihi 24 Ocak 2025'tir. Burada yer alan bilgi ve analizler tamamen kişisel çalışma olup kesin bir doğruluk içermemekte ve yatırım tavsiyesi içermemektedir.*  
-        *TÜİK’in hesaplamasıyla uyumlu olması açısından Kasım ayının ilk 24 günündeki veriler dikkate alınmıştır.*</small>
+        *TÜİK’in hesaplamasıyla uyumlu olması açısından Aralık ayının ilk 24 günündeki veriler dikkate alınmıştır.*</small>
         """, unsafe_allow_html=True)
 
         # Hazırlayan bilgisi
