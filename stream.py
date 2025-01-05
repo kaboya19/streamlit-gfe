3ffe91fe13a6541774de7e7cec03c31be466a3f0
Bora Kaya <kayabo19@itu.edu.tr>
2025-01-05 16:23:09 +0300

    .

--- stream.py
+++ stream.py
@@ -895,6 +895,7 @@ if page=="Gıda Fiyat Endeksi":
 
 
         endeksler = endeksler[sira]
+        endeksler=endeksler.drop("Değişim",axis=1)
         endeksler=endeksler.T.iloc[1:]
         endeksler=endeksler.set_index(pd.date_range(start="2024-10-11",freq="D",periods=len(endeksler)))
         endeksler.index=endeksler.index.strftime("%Y-%m-%d")
