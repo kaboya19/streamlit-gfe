e60d66e629fe1be38cf6ffa3e9e2bf3a94f90f6e
Bora Kaya <kayabo19@itu.edu.tr>
2025-01-20 10:54:00 +0300

    .

--- stream.py
+++ stream.py
@@ -2181,7 +2181,11 @@ if page=="Özel Kapsamlı Endeksler":
 
     # X ekseni ayarları (45 derece döndürme, kalın font)
     fig.update_xaxes(tickangle=45, tickfont=dict(size=12, family="Arial Black"))
-    
+    for i in range(1, tüik.shape[1] + 1):
+        current_range = fig.layout.yaxis.range if i == 1 else fig.layout[f"yaxis{i}"].range  # Mevcut y ekseni aralığını al
+        if current_range:
+            new_range = [current_range[0] - 2, current_range[1] + 2]  # 1 birim aşağı, 2 birim yukarı kaydır
+            fig.update_yaxes(range=new_range, row=i, col=1)
 
     st.plotly_chart(fig)
    
