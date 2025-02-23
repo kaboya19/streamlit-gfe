c59f2146288a989b2fb4333ffa48cef0d1f2b02c
Bora Kaya <kayabo19@itu.edu.tr>
2025-02-24 01:03:39 +0300

    update

--- mailsub.py
+++ mailsub.py
@@ -16,7 +16,7 @@ SENDER_EMAIL = "borakaya8@gmail.com"
 SENDER_PASSWORD = "dqpp vgar wujr vhei"
 
 # Abone listesi
-SUBSCRIBERS_FILE = "subscribers.csv"
+SUBSCRIBERS_FILE = "subscribers1.csv"
 
 # E-Posta Gönderim Fonksiyonu
 def send_email_with_images(to_email, subject, body, images):
@@ -66,43 +66,46 @@ def send_bulk_email_with_images(subject, body, images):
 
 # Ana E-Posta İçeriği ve Gönderimi
 if __name__ == "__main__":
-    subject = f"Web Gıda Fiyat Endeksi Aralık 2024 Bülteni"
+    subject = f"Web-Tüketici Fiyat Endeksi Şubat 2025 Bülteni"
     body = f"""
-    <h2 style='color:black; font-weight:bold;'>Web Gıda Fiyat Endeksi Aralık 2024 Bülteni</h2>
-    <h3 style='color:red; font-weight:bold;'>Web Gıda Fiyat Endeksi Aralık'ta %3,41 arttı</h3>
-    <p><em>(Teknik notlara bültenin en aşağısından ulaşabilirsiniz)</em></p>
+    <h2 style='color:black; font-weight:bold;'>Web-Tüketici Fiyat Endeksi Şubat 2025 Bülteni</h2>
+    <h3 style='color:red; font-weight:bold;'>Web-Tüketici Fiyat Endeksi Şubat'ta %3,41 arttı</h3>
     <br>
     <img src="cid:image1" style="max-width:600px; margin: 10px 0;"/>
-    <p>Web Gıda Fiyat Endeksi Aralık'ta %3,41 artış kaydederken mevsimsellikten arındırılmış artış %2,85 oldu.</p>
-    <p>Sepette ağırlığı en yüksek ürünlere bakıldığında:</p>
-    <ul>
-        <li><strong>Domates</strong>: -%3,32 azalırken</li>
-        <li><strong>Ayçiçek Yağı</strong>: %4,42</li>
-        <li><strong>Kuzu Eti</strong>: %4,76</li>
-        <li><strong>Ekmek</strong>: %3,67</li>
-        <li><strong>Dana Eti</strong>: %3,43</li>
-        <li><strong>Yumurta</strong>: %5,97</li>
-        <li><strong>Tavuk Eti</strong>: %0,29 artış kaydetti.</li>
-    </ul>
+    <p>Web-Şubat Fiyat Endeksi Şubat'ta %3,41 artış kaydederken mevsimsellikten arındırılmış artış %2,85 oldu.</p>
     <br>
+    <p>En çok artış ve düşüş yaşanan maddeler:</p>
     <img src="cid:image2" style="max-width:600px; margin: 10px 0;"/>
+    <p>En çok artış ve düşüş yaşanan temel başlıklar:</p>
     <img src="cid:image3" style="max-width:600px; margin: 10px 0;"/>
+    <p>Özel Kapsamlı TÜFE Göstergeleri:</p>
     <img src="cid:image4" style="max-width:600px; margin: 10px 0;"/>
+    <p>Özel Kapsamlı Göstergeler aylık artış oranları: </p>
     <img src="cid:image5" style="max-width:600px; margin: 10px 0;"/>
+    <p>Ana gruplara ait artış oranları:  </p>
     <img src="cid:image6" style="max-width:600px; margin: 10px 0;"/>
     <img src="cid:image7" style="max-width:600px; margin: 10px 0;"/>
     <img src="cid:image8" style="max-width:600px; margin: 10px 0;"/>
     <img src="cid:image9" style="max-width:600px; margin: 10px 0;"/>
-    <br>
-    <p>Mevsimsellikten arındırılmış olarak fiyat değişimlerinin ortalaması %2,72 ve medyan artış %2,80 olmuştur.</p>
-    <p>Meyve ve Sebze hariç fiyat artışı %2,85 ile manşet ile aynı seviyededir.</p>
-    <p>SATRIM(Mevsimsel Düzeltilmiş Budanmış Enflasyon) göstergesi ise %2,50 artmıştır.</p>
-    <br>
     <img src="cid:image10" style="max-width:600px; margin: 10px 0;"/>
-    <p><a href='https://web-gfe.streamlit.app'>Gıda Fiyat Endeksi ile ilgili tüm verilere buradan ulaşabilirsiniz.</a></p>
+    <img src="cid:image11" style="max-width:600px; margin: 10px 0;"/>
+    <img src="cid:image12" style="max-width:600px; margin: 10px 0;"/>
+    <img src="cid:image13" style="max-width:600px; margin: 10px 0;"/>
+    <img src="cid:image14" style="max-width:600px; margin: 10px 0;"/>
+    <img src="cid:image15" style="max-width:600px; margin: 10px 0;"/>
+    <img src="cid:image16" style="max-width:600px; margin: 10px 0;"/>
+    <br>
+    <p>Seçilmiş maddelere ait ortalama fiyatlar:</p>
+    <img src="cid:image17" style="max-width:600px; margin: 10px 0;"/>
+    <br>
+    <p>Mevsimsellikten arındırılmış ana eğilimlere bakıldığında medyan artış %3,21 olmuştur.</p>
+    <p>SATRIM(Mevsimsel Düzeltilmiş Budanmış Enflasyon) göstergesi ise %3,27 artmıştır.</p>
+    <br>
+    <img src="cid:image18" style="max-width:600px; margin: 10px 0;"/>
+    <p><a href='https://web-tufe.streamlit.app/'>Web-Tüketici Fiyat Endeksi ile ilgili tüm verilere buradan ulaşabilirsiniz.</a></p>
     <hr>
     <small>
-        *Bu bültenin bir sonraki yayınlanma tarihi 24 Ocak 2025'tir. Burada yer alan bilgi ve analizler tamamen kişisel çalışma olup kesin bir doğruluk içermemekte ve yatırım tavsiyesi içermemektedir.*<br>
+        *Bu bültenin bir sonraki yayınlanma tarihi 24 Mart 2025'tir. Burada yer alan bilgi ve analizler tamamen kişisel çalışma olup kesin bir doğruluk içermemekte ve yatırım tavsiyesi içermemektedir.*<br>
         *TÜİK’in hesaplamasıyla uyumlu olması açısından ayın ilk 24 günündeki veriler dikkate alınmıştır.*
     </small>
     <br>
@@ -115,15 +118,24 @@ if __name__ == "__main__":
 
     # Görsellerin Yolları
     images = {
-        "image1": "grafikler/gfe_24-12-2024.png",
-        "image2": "grafikler/aylıkdegisim_24-12-2024.png",
-        "image3": "grafikler/gruplar_24-12-2024.png",
-        "image4": "grafikler/harcamasa24-12-2024.png",
-        "image5": "grafikler/haric_24-12-2024.png",
-        "image6": "grafikler/birim_24-12-2024.png",
-        "image7": "grafikler/birimdüşen_24-12-2024.png",
-        "image8": "grafikler/kümülatif_24-12-2024.png",
-        "image9": "grafikler/egilim_24-12-2024.png"
+        "image1": "anagruplar.png",
+        "image2": "maddeler.png",
+        "image3": "temelbaşlıklar.png",
+        "image4": "özelgöstergeler.png",
+        "image5": "özelgöstergelerartış.png",
+        "image6": "eveşyası.png",
+        "image7": "eğitim.png",
+        "image8": "eğlence.png",
+        "image9": "Giyim ve ayakkabı.png",
+        "image10": "Gıda ve alkolsüz içecekler.png",
+        "image11": "Haberleşme.png",
+        "image12": "Konut.png",
+        "image13": "Lokanta ve oteller.png",
+        "image14": "Ulaştırma.png",
+        "image15": "Çeşitli mal ve hizmetler.png",
+        "image16": "Giyim ve ayakkabı.png",
+        "image17": "fiyatlar.png",
+        "image18": "eğilim.png"
     }
 
     # Toplu E-Posta Gönderimi
